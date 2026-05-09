9cec3b53440797fa7f613c73ed19f617836f9d6f
Alain Kräupl <alainkraupl@gmail.com>
2026-05-09 10:08:37 +0200

    refactor: add GENERATE_SBOM constant

--- backend/src/main_graph/subgraphs/discovery/constants.py
+++ backend/src/main_graph/subgraphs/discovery/constants.py
@@ -1,5 +1,6 @@
 """Node name constants for the ProjectDiscovery subgraph."""
 
 FETCH_REPOSITORY = "fetch_repository"
-PARSE_PACKAGE_FILES = "parse_package_files"
+GENERATE_SBOM = "generate_sbom"
+PARSE_PACKAGE_FILES = "parse_package_files"  # removed in Task 8
 BUILD_DEPENDENCY_SUMMARY = "build_dependency_summary"
